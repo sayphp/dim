444371cab0813c9dce9ab36103b0877b1e292ddc
liuxiao <liuxiao@sanhao.com>
2018-04-27 19:46:05 +0800

    保存一版，继续进行重构

---
+++ interface/act.interface.php
@@ -0,0 +1,13 @@
+<?php
+    /*
+     * actInterface
+     *
+     */
+    interface actInterface{
+
+        public function sign();//登陆
+
+        public function msg();//消息
+
+        public function quit();//退出
+    }
---
+++ task/server.task.php
@@ -0,0 +1,30 @@
+<?php
+    /**
+     * 服务器任务类
+     * serverTask.cls.php
+     * say
+     * 2018-04-27
+     */
+    class serverTask{
+        //运行
+        public function run(){
+            while(true){
+                //1. 服务状态检查
+                request(raft::$id, task::status());
+                echo 'server status check'.time().PHP_EOL;
+                //2. 分发数据落地服务
+                echo 'publish task'.time().PHP_EOL;
+                //3. 自定义任务处理
+                echo 'custom task'.time().PHP_EOL;
+                sleep(5);
+            }
+        }
+        //服务器状态检查
+        public function status(){
+            echo '状态正常'.PHP_EOL;
+        }
+        //服务器数据落地
+        public function land(){
+
+        }
+    }
